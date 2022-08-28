99a8c5c5522d0376311d7981f81c8dcee139bd5b
hbs30832 <hbs9312@gmail.com>
2022-08-29 02:49:41 +0900

    자동 배포 테스트

--- src/custom-axios.js
+++ src/custom-axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzUxMzgyOGY0MGU4YTgwNzMxYjBhN2Y1YTUwMjExNSIsInN1YiI6IjVlOGE3YjhiYjE4ZjMyMDAxM2UyZDUwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Uqfn7ygDJUCKh_TZhcuGR2qIVxYRJD8QSLGyP9XwfcM",
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`,
   },
 });
 
